b8a8e19b237ebf88c3f72f8ae51f2fd49d7c8184
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 19:37:23 +0200

    Inherit default charset and collation

--- data/upgrade/001-description-to-body.sql
+++ data/upgrade/001-description-to-body.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `musicians` CHANGE `description` `body` TEXT  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL;
-ALTER TABLE `record_labels` CHANGE `description` `body` TEXT  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL;
-ALTER TABLE `records` CHANGE `description` `body` TEXT  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL;
+ALTER TABLE `musicians` CHANGE `description` `body` TEXT  NULL;
+ALTER TABLE `record_labels` CHANGE `description` `body` TEXT  NULL;
+ALTER TABLE `records` CHANGE `description` `body` TEXT  NOT NULL;
